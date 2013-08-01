1a7646283c2bf29d15e8b1ea3e9d1804ccb47751
shaosohu <ios2007@163.com>
2013-08-01 18:12:39 +0800

    test2

--- MyGitTest/src/GitTest01.java
+++ MyGitTest/src/GitTest01.java
@@ -1,7 +1,8 @@
-/*web在线编辑*/
+﻿/*web在线编辑*/
 public class GitTest01 {
 
 	public static void main(String[] args) {
+		/*本地测试2*/
 		// TODO Auto-generated method stub
 		System.out.println("Test Git");
 	}
